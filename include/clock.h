d8c39cfe6cd33b4f91dc5cd9d12d9e423b61f4c0
Icaro-Cavalcante <icarodev06@gmail.com>
2026-07-04 22:14:51 -0300

    docs: Comentários no clock.h
    - `Adição de comentários no clock.h`

--- include/clock.h
+++ include/clock.h
@@ -1,14 +1,26 @@
+/* Módulo clock: O relógio global 
+* - Sincroniza o tempo
+* - Veículos se movem a cada tick
+* - O mapa é renderizado a cada tick
+*/
+
 #ifndef CLOCK_H
 #define CLOCK_H
 
 #include <pthread.h>
 
+/* Struct do relógio
+* tick -> medida de tempo usada
+* mutex -> o próprio mutex
+* cond -> a variável de condição
+*/
 typedef struct {
     int tick;
     pthread_mutex_t mutex;
     pthread_cond_t cond;
 } ClockState;
 
+/* Assinaturas das funções */
 void clock_init(void);
 
 void clock_destroy(void);
