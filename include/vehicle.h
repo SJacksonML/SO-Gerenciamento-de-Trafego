c0b7386fabe6d795de192c8107ce779b09bcc124
Elilucio7 <eliluciofilho@gmail.com>
2026-07-05 18:49:12 -0300

    fix: comments and remove
    adição de comentários simples mais remoção de número máximo de veículos

--- include/vehicle.h
+++ include/vehicle.h
@@ -1,18 +1,47 @@
-#define MAX_VEHICLE 30
 
-int VehiclesCurrently = 0;
+/*número máximo de veículos, deve ser respeitado na implementação para evitar engarrafamentos numerosos*/
+#define MAX_VEHICLE 10
 
+/*enum dos tipos de veículo*/
 typedef enum{
     TYPE_AMBULANCE,
     TYPE_CAR,
 }VehicleType;
 
+/*enum das possíveis velocidades de um veículo*/
 typedef enum{
     SPEED_SLOW,
     SPEED_MEDIUM,
     SPEED_FAST,
 }Speed;
 
+/*
+*struct de posição atual de um veículo, possui:
+*int row;
+*int col;
+*/
 typedef struct Pos Pos;
 
+/*
+*struct de um veículo, possui:
+*int id;
+*VehicleType type;
+*Direction direction;
+*Speed speed;
+*Pos pos;
+*Map *map;
+*/
 typedef struct Vehicle Vehicle;
+
+/*
+*função para alocar dados de um veículo
+*args: Vehicle *vehicle, int id, VehicleType t, Direction d, Pos start, Speed s, Map *m
+*retorna: vehicle (ponteiro entregue à função que pertence ao veículo)
+*/
+Vehicle *vehicle_init(Vehicle *vehicle, int id, VehicleType t, Direction d, Pos start, Speed s, Map *m);
+
+/*
+*função principal de vida de um veículo, usada para iniciar as threads.
+*args: Vehicle *vehicle
+*/
+void vehicle_run(Vehicle *vehicle);
