4dab3eea25df15e2795290053ee0a59b91e7b8e5
Elilucio7 <eliluciofilho@gmail.com>
2026-07-05 18:35:17 -0300

    feat: initial advance and run (2/2)
    finalização inicial de car_advance e vehicle_run, até agora com tratamento para células do mapa que alteram direção. conversas com a equipe sobre casos de interseção e partes da rua sem redirecionamento serão necessárias

--- src/vehicle.c
+++ src/vehicle.c
@@ -25,14 +25,12 @@ struct Vehicle{
 static bool should_move(Vehicle *vehicle){
     if (!vehicle) return false;
 
-    if (map_is_valid_move){
-        int tick = clock_get_tick();
-        switch (vehicle->speed){
-            case SPEED_FAST: return true;
-            case SPEED_MEDIUM: return (tick % 2 == 0);
-            case SPEED_SLOW: return (tick % 4 == 0);
-            default: false;
-        }
+    int tick = clock_get_tick(); //conversa necessária, sem acesso ao clock
+    switch (vehicle->speed){
+        case SPEED_FAST: return true;
+        case SPEED_MEDIUM: return (tick % 2 == 0);
+        case SPEED_SLOW: return (tick % 4 == 0);
+        default: false;
     }
 }
 
@@ -46,7 +44,8 @@ static Direction dir_from_tile(Vehicle *vehicle){
         case CELL_ONE_WAY_E: return DIR_EAST;
         case CELL_ONE_WAY_W: return DIR_WEST;
         case CELL_INTERSECTION: return DIR_COUNT;
-        default: return DIR_COUNT;
+        case CELL_ROAD: return DIR_COUNT;
+        default: return DIR_INVALID;
     }
 }
 
@@ -64,17 +63,31 @@ static Pos car_next_pos(Vehicle *vehicle){
     return next;
 }
 
-static void car_advance(Vehicle *vehicle, Pos next){
+static bool car_advance(Vehicle *vehicle, Pos next){
+    if (!should_move(vehicle)) return false;
     Direction check = map_is_valid_move(vehicle->map, next.row, next.col, vehicle->direction);
 
-    if (check != 0){
-        switch (check){
-            //case DIR_INVALID: break; /* for better case handling
+    switch (check){
+        case DIR_INVALID: break; // * invalid movement
 
-            case DIR_COUNT: break; // *signal/intersection treatment
-            
-            default:
+        case DIR_COUNT: break; // *signal/intersection treatment
+        /*
+        * waiting updates on sync for correct implementation
+        *TrafficSignal *TL = sync_get_signal(next.row, next.col); 
+        *if (vehicle->type == TYPE_AMBULANCE) traffic_request_priority(next.row, next.col, vehicle->direction);
+        *traffic_wait_green(next.row, next.col, vehicle->direction);
+        *if (cell_try_occupy(vehicle->map, next.row, next.col, vehicle->id)){
+        *    vehicle->pos = next;
+        *    cell_release(vehicle->map, next.row, next.col);
+        *    return true;
+        */
+        
+        default: // * handling direction-swap tiles
+        if (cell_try_occupy(vehicle->map, next.row, next.col, vehicle->id)){
             vehicle->pos = next;
+            cell_release(vehicle->map, next.row, next.col);
+            vehicle->direction = check;
+            return true;
         }
     }
     return false;
@@ -94,14 +107,21 @@ Vehicle *vehicle_init(Vehicle *vehicle, int id, VehicleType t, Direction d, Pos 
 }
 
 void vehicle_run(Vehicle *vehicle){
+    int current_tick;
     Map *map = vehicle->map;
     while (1/*change to ticks*/){
+        current_tick = clock_get_tick; //conversa necessária, não há acesso ao clock
         Pos pos = vehicle->pos;
         Direction tile_dir = dir_from_tile(vehicle);
+
         switch (tile_dir){
-            case DIR_COUNT: continue;
-            default: Pos next = car_next_pos(vehicle);
+            case DIR_INVALID: break; // *possible error treatment
+
+            case DIR_COUNT: break; // *waiting for sync updates
+
+            default: Pos next = car_next_pos(vehicle); // *handling the position advancement in direction-swap tiles
             car_advance(vehicle, next);
             }
+        clock_wait_tick(); // *waiting for clock to broadcast
         }
 }
